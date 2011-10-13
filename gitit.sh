6a196b38d35b456c2f627139a62270f477f32370
Joshua Smith <kernelsmith@kernelsmith.com>
2011-10-13 18:20:51 -0400

    cleaned up, added some dirs

--- gitit.sh
+++ gitit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! [ $# == 1 ]; then echo "[!] Usage $0 commit_message" && exit 1;fi
+if [ $# -ne 1 ]; then echo "[!] Usage $0 commit_message" && exit 1;fi
 
 git add *
 git commit -m "$1"
