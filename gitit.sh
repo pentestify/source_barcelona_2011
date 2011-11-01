14e5cc914ee266115046b6b4b8eb69cdb5c1ed8c
Joshua Smith <kernelsmith@kernelsmith.com>
2011-11-01 19:01:15 -0400

    updated gitit.sh to first to a git-pull

--- gitit.sh
+++ gitit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then echo "[!] Usage $0 commit_message" && exit 1;fi
+echo "[*] Updating the repo from master before committing"
+git pull
 
-# Should we add a git pull right here?
+if [ $# -ne 1 ]; then echo "[!] Usage $0 commit_message" && exit 1;fi
 
 git add *
 git commit -m "$1"
